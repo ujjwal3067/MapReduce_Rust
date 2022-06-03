287b92a539a18a583796d6a26d9a599da65b75f5
ujjwal <ujjwal.kumar@mail.mcgill.ca>
2022-06-02 21:24:41 -0400

    threadpool bug not fixed

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,30 @@ mod container;
 mod tasks;
 mod threadpool;
 mod user_input;
+use std::thread ; 
+use std::time ; 
 
 fn main() {
-    let mut container = container::Container::new(10);
-    tasks::map("test_resource/file1.txt".to_string(), &mut container);
-    for i in 0..container.get_size() {
-        println!("-----------------------------------------------------------------------");
-        println!("partition : {}", i);
-        tasks::reducer(i, &mut container);
-        println!("-----------------------------------------------------------------------");
+    // let mut container = container::Container::new(10);
+    // tasks::map("test_resource/file1.txt".to_string(), &mut container);
+    // for i in 0..container.get_size() {
+    //     println!("-----------------------------------------------------------------------");
+    //     println!("partition : {}", i);
+    //     tasks::reducer(i, &mut container);
+    //     println!("-----------------------------------------------------------------------");
+    // }
+
+
+    // TESING threadpool
+
+    use threadpool::pool    as pool ; 
+    let pool = pool::Threadpool::new(10); 
+    for i in 0..1000 { 
+        // Captures the variables i by moving it into the closure
+        pool.execute(move || {
+            println!("running : {}", i);
+        });
     }
+    //BUG : not all the jobs are finished before programs terminates
+    thread::sleep(time::Duration::from_secs(3)); 
 }
--- src/threadpool/pool.rs
+++ src/threadpool/pool.rs
@@ -5,23 +5,29 @@ use crate::container;
 use crate::tasks;
 use crate::threadpool;
 use std::sync::mpsc;
+use std::sync::Arc;
+use std::sync::Mutex;
 use std::thread;
-
 /// we want the worker structs to fetch jobs from a queue that the Threadpool  holds and run
 /// and run these jobs
 ///
 /// [basic logic]
 /// Threadpool holds sending side of the channel
-/// Worker thread holds receving side of the channel 
+/// Worker thread holds receving side of the channel
 ///
 ///A new job struct will hold the closure we want to send down the channel
 /// and then execute method of the threadpool will send the job it wants to execute down the
 /// sending side of the channel
 /// In a thread, the worker will loop over its receiving side of the channel and execute the
 /// closures of any jobs it executes.
+/// NOTE : receiver is shared between mutlipler worker thread via Arc<Mutex<T>> smart pointer
 pub struct Threadpool {
     workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
+
+/// closure  + static type  + safe to move between threads
+type Job = Box<dyn FnOnce() + Send + 'static>;
 
 impl Threadpool {
     /// Creates a new threadpool
@@ -33,12 +39,24 @@ impl Threadpool {
     /// The `new` function will panic is the size <= 0  
     pub fn new(size: usize) -> Threadpool {
         assert!(size > 0);
+        let (sender, receiver) = mpsc::channel();
+        // so that receiver can be shared between multiple worker threads for picking up jobs for execution 
+        let receiver = Arc::new(Mutex::new(receiver));  
         let mut workers = Vec::with_capacity(size);
         for id in 0..size {
-            workers.push(Worker::new(id));
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
-        Threadpool { workers }
+        Threadpool { workers, sender }
     }
+
+
+    pub fn execute<F>(&self, f : F) 
+        where 
+            F : FnOnce() + Send + 'static
+        { 
+            let job = Box::new(f); 
+            self.sender.send(job).unwrap();
+        }
 }
 
 struct Worker {
@@ -47,8 +65,19 @@ struct Worker {
 }
 
 impl Worker {
-    fn new(id: usize) -> Worker {
-        let thread = thread::spawn(|| {});
-        Worker { id, thread }
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        // we are sending recevier to worker thread ends for executing jobs
+        let thread = thread::spawn(move|| {
+            loop { 
+                // this is blocking operation
+                let job = receiver.lock().unwrap().recv().expect("[Error] : There was an error on the receiver side of the channel"); 
+                println!("Worker {} got a job : executing.", id); 
+                job(); // execute the closure
+            }
+        });
+        Worker { 
+            id,
+            thread, 
+        }
     }
 }
